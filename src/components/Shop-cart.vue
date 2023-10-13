852b30dc3eb6464c06238cad838369ff88c8067b
zenitovich <zenitovich@gmail.com>
2023-10-13 22:37:29 +0300

    added Shop-cart component and get fetch with shop-carts data

---
+++ src/components/Shop-cart.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="shop-cart">
+        <div class="shop-cart__name"></div>
+        <div class="shop-cart__sale"></div>
+        <div class="shop-cart__new"></div>
+        <div class="shop-cart__description"></div>
+        <div class="shop-cart__img"></div>
+        <div class="shop-cart__price"></div>
+        <div class="shop-cart__old-price"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Shop-cart"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/shop.vue
+++ src/views/shop.vue
@@ -6,6 +6,10 @@
               <div class="shop-header__filter--border"></div>
               <div class="shop-header__filter--results">32 results</div>
           </div>
+          <div class="shop-carts">
+              {{getShopCart()}}
+              <shop-cart></shop-cart>
+          </div>
           <div class="shop-header__sort">
               <div class="shop-header__sort--text"> Short by</div>
               <div class="shop-header__sort--input"><input type="text" placeholder="Default" class="shop__sort--input-input"></div>
@@ -23,8 +27,32 @@
 </template>
 
 <script>
+import ShopCart from "@/components/Shop-cart.vue";
+
 export default {
   name: "shop-page",
+  components: {ShopCart},
+
+  data() {
+    return {
+      ArrOfCarts: [],
+    }
+  },
+
+  //or computed if we not use data keys
+  methods: {
+    getShopCart() {
+      fetch('http://localhost:4000/items/')
+        .then((response) => {
+          return response.json();
+        })
+        .then((items) => {
+          this.ArrOfCarts = JSON.stringify(items)
+          console.log(JSON.stringify(items))
+        });
+    }
+  },
+
   beforeCreate() {
     this.$route.meta.layout = "DefaultLayout"
   }
